9880168b431630bc329bad09aa42d130c28b797f
Night66fr <158843492+Night66fr@users.noreply.github.com>
2025-12-29 00:45:55 +0100

    web

--- webserver.py
+++ webserver.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
+
 @app.route('/')
 def home():
-  return 'discord bot is fine'
+    return 'discord bot is fine'
 
 def run():
-  app.run(host='0.0.0.0', port = 8080)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get("PORT", 3000))
+    )
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+    Thread(target=run).start()
